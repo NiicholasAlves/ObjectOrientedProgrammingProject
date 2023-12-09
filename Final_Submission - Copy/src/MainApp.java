d8d1c087bae17498069189fb2e30000dee55c838
Nykyta <60669163+Nyko77771@users.noreply.github.com>
2023-12-09 17:33:54 -0300

    Update MainApp.java

--- Final_Submission - Copy/src/MainApp.java
+++ Final_Submission - Copy/src/MainApp.java
@@ -8,53 +8,12 @@ import java.io.FileNotFoundException;
  */
 public class MainApp {
     
-        public static void main(String[] args) throws FileNotFoundException {
-        
-//        QuestionsFactory.loadQuestions();  - A TEST CODE.           
+        public static void main(String[] args) throws FileNotFoundException {          
 
-        QuestionsGUI myGUI = new QuestionsGUI();
+        //Launches Matiases section. It is the opening page
+        ClimateDataGUI myGUI = new ClimateDataGUI();
         myGUI.setVisible(true);     
-                   
-//        generateQuestions(); - A test code
          
-        }
-         
-        // TESTING ARRAYLIST.
-//       public static void generateQuestions(){
-//        
-//        QuestionsList questions = QuestionsList.getINSTANCE();
-//        
-//        int i = 5;
-//        Scanner s = new Scanner(System.in);
-//        CorrectAndWrong correct = new CorrectAndWrong();
-//        
-//        while(i > 0){
-//            
-//            Question randomQuestion = questions.getRandom();
-//            System.out.println(randomQuestion.getQuestion());
-//            System.out.println("What is your answer?");
-//            boolean uChoice = s.nextBoolean();
-//            
-//            if(uChoice == randomQuestion.getAnswer()){
-//                System.out.println("Correct!");
-//                questions.removeQuestion
-//        (randomQuestion);
-//                correct.addCorrect();
-//            } else{
-//                System.out.println("Wrong choice");
-//                System.out.println(randomQuestion.getFact());
-//                questions.removeQuestion
-//        (randomQuestion);
-//                correct.addWrong();
-//            }
-//            i--;
-//       }
-//        
-//            System.out.println("You got total Correct Answers: " + correct.getCorrect());
-//            System.out.println("You got total wrong Answers: " + correct.getWrong());
-//
-//    }
-        
-        
-    
+        }           
 }
+
